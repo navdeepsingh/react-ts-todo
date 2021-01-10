bc669af4d5ff37dc32f7dfd6b91515a07dfe3658
Navdeep Singh <navdeep.er@gmail.com>
2021-01-10 13:00:57 +0530

    Setup basic Todo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+interface ITodo {
+  id: number
+  text: undefined | string
+  done: boolean
+}
+
+const Todo = () => {
+  const [todos, setTodos] = React.useState<ITodo[]>([])
+  const todoRef = React.useRef<HTMLInputElement | null>(null)
+
+  const handleSubmit = (e: React.SyntheticEvent): void => {
+    e.preventDefault()
+    
+    const newTodos = todos.slice() // Copy
+    newTodos.push({
+      id: newTodos.length + 1,
+      text: todoRef.current?.value,
+      done: false,
+    })
+
+    setTodos(newTodos)
+  }
+
+  const handleToggle = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const selectedId = parseInt(
+      e.target.id.substr(e.target.id.indexOf('-') + 1),
+    )
+
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === selectedId) {
+        return {id: todo.id, text: todo.text, done: !todo.done}
+      }
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+
+  const handleClearAll = (e: React.FormEvent<HTMLButtonElement>): void => {
+    e.preventDefault()
+
+    const updateddTodos = todos.map(todo => {
+      return {id: todo.id, text: todo.text, done: true}
+    })
+
+    setTodos(updateddTodos)
+  }
+
+  return (
+    <>
+      <h1>Todos List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="todo" ref={todoRef} />
+      </form>
+      <button onClick={handleClearAll}>Clear all todos</button>
+      <ul>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                id={`todo-${todo.id}`}
+                onChange={handleToggle}
+                checked={todo.done}
+                required
+              />
+              <label
+                htmlFor={`todo-${todo.id}`}
+                style={{textDecoration: todo.done ? 'line-through' : undefined}}
+              >
+                {todo.text}
+              </label>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
